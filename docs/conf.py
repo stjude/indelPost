8009001bb07a0cb29100c79f1096ca66ecce9e5b
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2021-05-20 14:30:41 -0500

    doc test

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ import sys
 _libdir = "../build/lib.%s-%s-%s.%s" % (os.uname()[0].lower(), os.uname()[4],
                                         sys.version_info[0], sys.version_info[1])
 
+print(_libdir)
+
 if os.path.exists(_libdir):
     sys.path.insert(0, os.path.abspath(_libdir))
 
