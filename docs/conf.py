215707854fde71e7f784200a998bb52d300a06a8
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2021-05-20 13:09:53 -0500

    add path-to-build in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import sys
 _libdir = "../build/lib.%s-%s-%s.%s" % (os.uname()[0].lower(), os.uname()[4],
                                         sys.version_info[0], sys.version_info[1])
 
-if sys.version_info[0], sys.version_info[1]):
+if os.path.exists(_libdir):
     sys.path.insert(0, os.path.abspath(_libdir))
 
 
