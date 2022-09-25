201ab95c857a9b68fdbad2e9e8207e606a01ad03
Ibrahim Numanagić <ibrahimpasa@gmail.com>
2022-09-25 09:19:25 -0700

    Integrate latest ssw-py with ARM support; Refactor and remove incompatible ssw-py calls

--- setup.py
+++ setup.py
@@ -1,70 +1,81 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from pysam import get_include as pysam_get_include
+import os
 
-def pip_install(pkg_name):
-    import subprocess
+# ssw-py: source files to include in installation for tar.gz
+PACKAGE_PATH = os.path.abspath(os.path.dirname(__file__))
+MODULE_PATH = os.path.join(PACKAGE_PATH, "ssw")
+LIB_PATH = os.path.join(MODULE_PATH, "lib")
+ssw_files = [
+    os.path.relpath(os.path.join(root, f), MODULE_PATH)
+    for root, _, files in os.walk(LIB_PATH)
+    for f in files
+    if ((".h" in f) or (".c" in f) or (".cpp" in f))
+]
 
-    subprocess.check_call(
-        ["python", "-m", "pip", "install", pkg_name], stdout=subprocess.DEVNULL
-    )
-
-
-try:
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext
-except ImportError:
-    pip_install("cython")
-
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext
-
-try:
-    from pysam import get_include as pysam_get_include
-except ImportError:
-    pip_install("pysam")
-    from pysam import get_include as pysam_get_include
-
-try:
-    import numpy
-except ImportError:
-    pip_install("numpy")
-
-
+extra_compile_args = ["-Wno-unused-function"]
 extensions = [
     Extension(
         "indelpost.variant",
         ["indelpost/variant.pyx"],
         include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
         "indelpost.utilities",
         ["indelpost/utilities.pyx"],
         include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
-        "indelpost.pileup", ["indelpost/pileup.pyx"], include_dirs=pysam_get_include(),
+        "indelpost.pileup",
+        ["indelpost/pileup.pyx"],
+        include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
-        "indelpost.varaln", ["indelpost/varaln.pyx"], include_dirs=pysam_get_include(),
+        "indelpost.varaln",
+        ["indelpost/varaln.pyx"],
+        include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
-        "indelpost.contig", ["indelpost/contig.pyx"], include_dirs=pysam_get_include(),
+        "indelpost.contig",
+        ["indelpost/contig.pyx"],
+        include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
-        "indelpost.local_reference", ["indelpost/local_reference.pyx"], include_dirs=pysam_get_include(),
+        "indelpost.local_reference",
+        ["indelpost/local_reference.pyx"],
+        include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
         "indelpost.softclip",
         ["indelpost/softclip.pyx"],
         include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
-        "indelpost.localn", ["indelpost/localn.pyx"], include_dirs=pysam_get_include(),
+        "indelpost.localn",
+        ["indelpost/localn.pyx"],
+        include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
     ),
     Extension(
         "indelpost.gappedaln",
         ["indelpost/gappedaln.pyx"],
         include_dirs=pysam_get_include(),
+        extra_compile_args=extra_compile_args,
+    ),
+    Extension(
+        "indelpost.sswpy",
+        sources=["indelpost/sswpy.pyx", "indelpost/ssw.c"],
+        extra_compile_args=extra_compile_args,
     ),
 ]
 
@@ -82,7 +93,8 @@ setup(
     author_email="kohei.hagiwara@stjude.org",
     license="Apache License 2.0",
     packages=find_packages(exclude=["tests"]),
+    package_data={"ssw": ssw_files},
     cmdclass={"build_ext": build_ext},
     ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
-    install_requires=["ssw-py", "numpy>=1.16.0", "pysam>=0.15.0", "cython>=0.29.12"],
+    install_requires=["numpy>=1.16.0", "pysam>=0.15.0", "cython>=0.29.12"],
 )
